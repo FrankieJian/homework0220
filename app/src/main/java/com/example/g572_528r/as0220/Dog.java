704f9fca57a9b865bc67b0778ec29b13c96c6da1
g572-528r <845828099@qq.com>
2017-02-20 20:46:55 +0800

    第一次提交

---
+++ app/src/main/java/com/example/g572_528r/as0220/Dog.java
@@ -0,0 +1,15 @@
+package com.example.g572_528r.as0220;
+
+/**
+ * Created by yls on 2017/2/20.
+ */
+public class Dog {
+    String name;
+    int imgId;
+
+    public  Dog(String name, int imgId){
+        this.name = name;
+        this.imgId = imgId;
+    }
+
+}
